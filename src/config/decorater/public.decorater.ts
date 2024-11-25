c6e25a1ead52ed3658f0375791d6b2ab8b0b6816
navnin2 <navneethnnair220@gmail.com>
2024-11-26 00:29:16 +0530

    public

--- src/config/decorater/public.decorater.ts
+++ src/config/decorater/public.decorater.ts
@@ -1,4 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
 
 export const IS_PUBLIC_KEY = 'isPublic';
-export const Public = () => SetMetadata(IS_PUBLIC_KEY, false);
+export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
